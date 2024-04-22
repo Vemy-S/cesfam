a96e0da80433e13cc70affa4efbb08dbe154aaf5
Jeremy Vega <jeredev20@gmail.com>
2024-04-22 00:25:16 -0400

    diego

--- public/API/loginuserRequest.js
+++ public/API/loginuserRequest.js
@@ -24,13 +24,9 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
             localStorage.setItem('token', token);
             console.log(token, 'token')
             
-           
-
-         
             console.log('Login successful');
             console.log(data); 
 
-            window.location.href = "http://127.0.0.1/4000/index.html"
         } else {
          
             console.error(data.message);
