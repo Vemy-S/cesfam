255c26858ec00e00c9b786b9fad61139a9957963
Damarys Silva <damarys.asg@gmail.com>
2024-04-29 15:02:36 -0400

    proteccion de rutas

--- public/API/routeProtection.js
+++ public/API/routeProtection.js
@@ -1,16 +1,11 @@
-// proteccionRutas.js
-
-// Verifica si el token de acceso está presente en el almacenamiento local
 function isAccessTokenPresent() {
-    return !!localStorage.getItem('accessToken');
+    return document.cookie.includes('token=');
 }
 
-// Función para redirigir al usuario a la página de inicio de sesión
 function redirectToLogin() {
-    window.location.href = '/login.html'; // Cambia '/login.html' por la ruta correcta de tu página de inicio de sesión
+    window.location.href = '/login.html'; //ruta  página de inicio de sesión
 }
 
-// Verifica si el token de acceso está presente al cargar la página
 window.addEventListener('DOMContentLoaded', () => {
     if (!isAccessTokenPresent()) {
         redirectToLogin();
