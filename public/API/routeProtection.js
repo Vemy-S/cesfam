8d679fc2e7e400c50b479a7e8b8336ee786965a4
Jeremy Vega <jeredev20@gmail.com>
2024-05-06 22:16:32 -0400

    data.message

--- public/API/routeProtection.js
+++ public/API/routeProtection.js
@@ -6,8 +6,15 @@ function redirectToLogin() {
     window.location.href = '/login.html'; //ruta  página de inicio de sesión
 }
 
+function redirectToIndex() {
+    window.location.href = '/inicio.html'; //ruta  página de inicio de sesión
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     if (!isAccessTokenPresent()) {
         redirectToLogin();
-    }
+    } 
 });
+
+
+
